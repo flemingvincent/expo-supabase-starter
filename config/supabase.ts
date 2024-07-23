626ef98a541ac7d77fb445271133154c02249485
Vincent Fleming <flemingvincent7@gmail.com>
2024-07-23 16:13:15 -0400

    Fixed SecureStore value size warning, updated dependencies.

--- config/supabase.ts
+++ config/supabase.ts
@@ -1,26 +1,14 @@
 import "react-native-url-polyfill/auto";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import * as SecureStore from "expo-secure-store";
-
-const ExpoSecureStoreAdapter = {
-	getItem: (key: string) => {
-		return SecureStore.getItemAsync(key);
-	},
-	setItem: (key: string, value: string) => {
-		SecureStore.setItemAsync(key, value);
-	},
-	removeItem: (key: string) => {
-		SecureStore.deleteItemAsync(key);
-	},
-};
 
 const supabaseUrl = process.env.EXPO_PUBLIC_API_URL as string;
 const supabaseKey = process.env.EXPO_PUBLIC_API_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
 	auth: {
-		storage: ExpoSecureStoreAdapter as any,
+		storage: AsyncStorage,
 		autoRefreshToken: true,
 		persistSession: true,
 		detectSessionInUrl: false,
