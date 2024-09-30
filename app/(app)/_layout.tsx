5d322f5f4ed85f93d452ce274efb8e6abfabb4a1
Vincent Fleming <flemingvincent7@gmail.com>
2024-09-30 19:04:59 -0400

    Added constants directory, updated header styles, updated tab bar styles, removed dependabot.

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from "expo-router";
 
+import { colors } from "@/constants/colors";
+import { useColorScheme } from "@/lib/useColorScheme";
+
 export const unstable_settings = {
 	initialRouteName: "(root)",
 };
 
 export default function AppLayout() {
+	const { colorScheme } = useColorScheme();
+
 	return (
 		<Stack screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="(protected)" />
@@ -15,6 +20,16 @@ export default function AppLayout() {
 					presentation: "modal",
 					headerShown: true,
 					headerTitle: "Sign Up",
+					headerStyle: {
+						backgroundColor:
+							colorScheme === "dark"
+								? colors.dark.background
+								: colors.light.background,
+					},
+					headerTintColor:
+						colorScheme === "dark"
+							? colors.dark.foreground
+							: colors.light.foreground,
 				}}
 			/>
 			<Stack.Screen
@@ -23,6 +38,16 @@ export default function AppLayout() {
 					presentation: "modal",
 					headerShown: true,
 					headerTitle: "Sign In",
+					headerStyle: {
+						backgroundColor:
+							colorScheme === "dark"
+								? colors.dark.background
+								: colors.light.background,
+					},
+					headerTintColor:
+						colorScheme === "dark"
+							? colors.dark.foreground
+							: colors.light.foreground,
 				}}
 			/>
 			<Stack.Screen
@@ -31,6 +56,16 @@ export default function AppLayout() {
 					presentation: "modal",
 					headerShown: true,
 					headerTitle: "Modal",
+					headerStyle: {
+						backgroundColor:
+							colorScheme === "dark"
+								? colors.dark.background
+								: colors.light.background,
+					},
+					headerTintColor:
+						colorScheme === "dark"
+							? colors.dark.foreground
+							: colors.light.foreground,
 				}}
 			/>
 		</Stack>
