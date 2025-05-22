44c7c80f107eb2d08fd96314c71b5a0f092a6aac
Vincent Fleming <flemingvincent7@gmail.com>
2025-05-22 11:35:49 -0400

    ESS-13: Use Expo Secure Store instead of AsyncStorage

--- config/supabase.ts
+++ config/supabase.ts
@@ -1,15 +1,60 @@
-import "react-native-url-polyfill/auto";
-
 import { AppState } from "react-native";
-import { createClient } from "@supabase/supabase-js";
+
+import "react-native-get-random-values";
+import * as aesjs from "aes-js";
+import * as SecureStore from "expo-secure-store";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
+class LargeSecureStore {
+	private async _encrypt(key: string, value: string) {
+		const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8));
+		const cipher = new aesjs.ModeOfOperation.ctr(
+			encryptionKey,
+			new aesjs.Counter(1),
+		);
+		const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value));
+		await SecureStore.setItemAsync(
+			key,
+			aesjs.utils.hex.fromBytes(encryptionKey),
+		);
+		return aesjs.utils.hex.fromBytes(encryptedBytes);
+	}
+	private async _decrypt(key: string, value: string) {
+		const encryptionKeyHex = await SecureStore.getItemAsync(key);
+		if (!encryptionKeyHex) {
+			return encryptionKeyHex;
+		}
+		const cipher = new aesjs.ModeOfOperation.ctr(
+			aesjs.utils.hex.toBytes(encryptionKeyHex),
+			new aesjs.Counter(1),
+		);
+		const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value));
+		return aesjs.utils.utf8.fromBytes(decryptedBytes);
+	}
+	async getItem(key: string) {
+		const encrypted = await AsyncStorage.getItem(key);
+		if (!encrypted) {
+			return encrypted;
+		}
+		return await this._decrypt(key, encrypted);
+	}
+	async removeItem(key: string) {
+		await AsyncStorage.removeItem(key);
+		await SecureStore.deleteItemAsync(key);
+	}
+	async setItem(key: string, value: string) {
+		const encrypted = await this._encrypt(key, value);
+		await AsyncStorage.setItem(key, encrypted);
+	}
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 	auth: {
-		storage: AsyncStorage,
+		storage: new LargeSecureStore(),
 		autoRefreshToken: true,
 		persistSession: true,
 		detectSessionInUrl: false,
